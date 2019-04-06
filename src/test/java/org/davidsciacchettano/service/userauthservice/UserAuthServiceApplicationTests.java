ab8c04234cf405cca4f4a4e69634345da7362a50
David Sciacchettano <david.sciacchettano@gmail.com>
2019-04-06 09:18:30 -0400

    Intital commit

---
+++ src/test/java/org/davidsciacchettano/service/userauthservice/UserAuthServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package org.davidsciacchettano.service.userauthservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UserAuthServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
